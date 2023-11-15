e020d0c13e01146b54acb33702faf283d7392cef
Szymon Kadaś <kadasszymon@gmail.com>
2023-11-15 13:44:56 +0100

    implemented beta version for scrapperClass

--- src/bot/scrapper/scrapper.ts
+++ src/bot/scrapper/scrapper.ts
@@ -1,3 +1,71 @@
-export class Scrapper {
-    // Scrapper should be a set of methods that you could use for navigating through scrapped website.
+import { Browser, Page } from "puppeteer";
+// jak doprowadzić do require i mieć typy?
+const puppeteer = require("puppeteer-extra");
+const stealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(stealthPlugin());
+interface ScrapperOptions {
+  searchValue: string;
+  maxRecords: number;
+}
+
+interface OptionalScrapperOptions {
+  searchValue?: string;
+  maxRecords?: number;
+}
+
+export default class Scrapper {
+  public searchValue: string;
+  public maxRecords: number;
+  public browser: Browser;
+  public page: Page;
+  public scrapedData: Object;
+  public html: string;
+  // Scrapper should be a set of methods that you could use for navigating through scrapped website.
+  constructor({ searchValue, maxRecords }: ScrapperOptions) {
+    this.changeConfig({ searchValue, maxRecords });
+  }
+
+  changeConfig = ({ searchValue, maxRecords }: OptionalScrapperOptions) => {
+    this.searchValue = searchValue !== undefined ? searchValue : this.searchValue;
+    this.maxRecords = maxRecords !== undefined ? maxRecords : this.maxRecords;
+  };
+
+  launch = async () => {
+    this.browser = await puppeteer.launch();
+    this.page = await this.browser.newPage();
+  };
+
+  navigate = async (navTo: string) => {
+    await this.page.goto(navTo);
+  };
+
+  scrape = async (properties: string[]) => {
+    this.html = await this.page.content();
+    const searchValue = this.searchValue;
+    const maxRecords = this.maxRecords;
+    const data = await this.page.evaluate(
+      (searchValue, maxRecords, properties) => {
+        const foundElements = Array.from(document.querySelectorAll(searchValue)).slice(0, maxRecords);
+        return foundElements.map((element) => {
+          const result = {};
+          properties.forEach((property) => {
+            result[property] = element[property];
+          });
+          return result;
+        });
+      },
+      searchValue,
+      maxRecords,
+      properties
+    );
+    this.scrapedData = data;
+  };
+
+  getData = () => {
+    return this.scrapedData;
+  };
+
+  close = async () => {
+    await this.browser.close();
+  };
 }
--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,9 +1,14 @@
-const findOffers = () => {
-    console.log('Scrapping...')
+import Scrapper from "../bot/scrapper/scrapper";
+const findOffers = async () => {
+  console.log("Scrapping...");
+  const ScrapperInstance = new Scrapper({ searchValue: ".main-content", maxRecords: 4 });
+  await ScrapperInstance.launch();
+  await ScrapperInstance.navigate("https://nofluffjobs.com/pl");
+  await ScrapperInstance.scrape(["innerHTML", "innerText"]);
+  const mak = await ScrapperInstance.getData();
+  setTimeout(() => {
+    console.log("10 offers found.");
+  }, 3000);
+};
 
-    setTimeout(() => {
-        console.log('10 offers found.')
-    }, 3000)
-}
-
-findOffers()
+findOffers();
