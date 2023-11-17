bec8c8163970e44b599371ffe747d09e9da2b65b
Szymon Kadaś <kadasszymon@gmail.com>
2023-11-17 21:25:41 +0100

    scrapper alpha version (did: navigation to offers with desired options)

--- src/bot/scrapper/scrapper.ts
+++ src/bot/scrapper/scrapper.ts
@@ -18,8 +18,9 @@ export default class Scrapper {
   public maxRecords: number;
   public browser: Browser;
   public page: Page;
-  public scrapedData: Object;
-  public html: string;
+  public scrapedData: string;
+  public linkDivider: string = ";link-divider;";
+
   // Scrapper should be a set of methods that you could use for navigating through scrapped website.
   constructor({ searchValue, maxRecords }: ScrapperOptions) {
     this.changeConfig({ searchValue, maxRecords });
@@ -31,34 +32,55 @@ export default class Scrapper {
   };
 
   launch = async () => {
-    this.browser = await puppeteer.launch();
+    this.browser = await puppeteer.launch({
+      headless: false,
+    });
     this.page = await this.browser.newPage();
   };
 
-  navigate = async (navTo: string) => {
-    await this.page.goto(navTo);
+  // reactionTime = () => {
+  //   return Math.max(482, Math.round(Math.random() * 1500));
+  // };
+
+  click = async (pathToElement: string, timeout = 10000) => {
+    // for unknown reasons without try catch node is detached from document.
+    try {
+      await this.page.click(pathToElement);
+    } catch (e) {
+      // Like this should be enought, but it has to be within try catch...
+      await this.page.waitForSelector(pathToElement, { timeout: timeout });
+      await this.page.click(pathToElement);
+    }
   };
 
-  scrape = async (properties: string[]) => {
-    this.html = await this.page.content();
-    const searchValue = this.searchValue;
-    const maxRecords = this.maxRecords;
+  type = async (pathToElement: string, text?: string) => {
+    await this.page.waitForSelector(pathToElement);
+    await this.page.type(pathToElement, text ? text : this.searchValue);
+    await this.page.keyboard.press("Enter");
+  };
+
+  navigate = async (navTo: string) => {
+    await this.page.goto(navTo, { waitUntil: "domcontentloaded", timeout: 3000 });
+  };
+
+  scrape = async (scrapePath: string, properties: string[]) => {
     const data = await this.page.evaluate(
-      (searchValue, maxRecords, properties) => {
-        const foundElements = Array.from(document.querySelectorAll(searchValue)).slice(0, maxRecords);
+      (scrapePath, maxRecords, properties, linkDivider) => {
+        const foundElements = Array.from(document.querySelectorAll(scrapePath)).slice(0, maxRecords);
         return foundElements.map((element) => {
-          const result = {};
-          properties.forEach((property) => {
-            result[property] = element[property];
+          return properties.map((property) => {
+            return element[property];
           });
-          return result;
         });
       },
-      searchValue,
-      maxRecords,
-      properties
+      scrapePath,
+      this.maxRecords,
+      properties,
+      this.linkDivider
     );
-    this.scrapedData = data;
+
+    this.scrapedData = data.join(this.linkDivider);
+    return data.join(this.linkDivider);
   };
 
   getData = () => {
--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,14 +1,60 @@
 import Scrapper from "../bot/scrapper/scrapper";
 const findOffers = async () => {
   console.log("Scrapping...");
-  const ScrapperInstance = new Scrapper({ searchValue: ".main-content", maxRecords: 4 });
+  const ScrapperInstance = new Scrapper({ searchValue: "frontend", maxRecords: 4 });
   await ScrapperInstance.launch();
-  await ScrapperInstance.navigate("https://nofluffjobs.com/pl");
-  await ScrapperInstance.scrape(["innerHTML", "innerText"]);
-  const mak = await ScrapperInstance.getData();
+  await ScrapperInstance.navigate("https://www.pracuj.pl/");
+  try {
+    // accept only necessary cookies
+    await ScrapperInstance.click("[data-test='button-customizeCookie']");
+    await ScrapperInstance.click("[data-test='button-submit']");
+  } catch (e) {
+    console.log("no need to accept cookies");
+  }
+  // zaznaczam it
+  await ScrapperInstance.click('[data-test="tab-item-it"]');
+  // wpisuję w searchbarze searchvalue zapisane w configu.
+  await ScrapperInstance.type(".core_fhefgxl");
+  // klikam searcha
+  await ScrapperInstance.click('[data-test="section-search-with-filters-it"] > .core_b1fqykql');
+  // zaznaczam widełki! Potrzebny jest full screen, inaczej trzeba będzie dodać odpowiednie kroki.
+  try {
+    await ScrapperInstance.click(".core_sm237sg", 5000);
+  } catch (e) {
+    await ScrapperInstance.click(".b95l1wx .core_brjyyp .core_b1fqykql");
+  }
+  // klikam searcha jeszcze raz.
+  await ScrapperInstance.click(".core_s1cjjpc4 .core_b1fqykql");
+  // posortuj wedle najbardziej pasujących.
+  try {
+    await ScrapperInstance.click(".listing_lkjndla", 5000);
+    await ScrapperInstance.click(".listing_covfkjq > .listing_o1bn1ih4:nth-of-type(2)", 5000);
+  } catch (e) {
+    await ScrapperInstance.click(".listing_l1b5wr8p");
+    await ScrapperInstance.click(".listing_onyjmg2:nth-of-type(2)");
+  }
+
+  // zgromadź linki
+  await ScrapperInstance.scrape('[data-test="section-offers"] div .core_n194fgoq', ["href"]);
+  // scrap location of description: ".offer-viewzxQhTZ"; Chociaż może lepiej rozbić i zrobić z tego porządniejszego jsona?
+  const links = await ScrapperInstance.getData();
+  await links.split(ScrapperInstance.linkDivider).forEach((link) => {
+    ScrapperInstance.navigate(link);
+    // logika brania rzeczy joł joł
+  });
   setTimeout(() => {
-    console.log("10 offers found.");
+    console.log(`${links.split(ScrapperInstance.linkDivider).length} offers found`);
   }, 3000);
 };
 
 findOffers();
+
+// przenieś potem ten kod do funkcji scrap first service.
+
+/*
+  const links = await ScrapperInstance.scrape('[data-test="section-offers"] div .listing-it_n194fgoq', ["href"]);
+  // scrap location of description: ".offer-viewzxQhTZ"; Chociaż może lepiej rozbić i zrobić z tego porządniejszego jsona?
+  await links.forEach((link) => {
+    console.log(link);
+  });
+*/
