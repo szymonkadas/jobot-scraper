67b83e255bd653863f88f393478c3c0c8fb189cd
Szymon Kadaś <kadasszymon@gmail.com>
2023-12-02 17:28:46 +0100

    did: implemented scheduling using node-schedule, and npm scripts

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -12,6 +12,7 @@ program.option("-s, --searchValue <string>", "javascript-developer");
 program.option("-l, --limitRecords <number>", "4");
 program.parse(process.argv);
 const options: ScrapperOptions = program.opts();
+// finding offers
 const findOffers = async (searchValue: string, limitRecords: number) => {
   console.log("Scrapping...");
   const fileID = getCurrentDateAsUUID();
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
-import { createServer, Server, ServerResponse } from 'http';
+import { createServer, Server, ServerResponse } from "http";
+const schedule = require("node-schedule");
+const { exec } = require("child_process");
 
 const PORT = 4200 || process.env.PORT;
 
 const server: Server = createServer((request, response: ServerResponse) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World!\n');
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World!\n");
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
+  schedule.scheduleJob("* * 9 * * 1-5", () => {
+    exec("npm run scrap:offers -- -s 'Javascript Developer' -l 30");
+  });
 });
