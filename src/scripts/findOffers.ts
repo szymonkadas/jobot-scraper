8bc8c6f485111534c68846998383629e62141159
Szymon Kadaś <kadasszymon@gmail.com>
2023-12-07 12:01:48 +0100

    implemented argument to distinct between saving data in json or csv

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -10,10 +10,13 @@ const { Command } = require("commander");
 const program = new Command();
 program.option("-s, --searchValue <string>", "javascript-developer");
 program.option("-l, --limitRecords <number>", "4");
+program.option("-d, --saveDataAsJson <boolean>", "true");
 program.parse(process.argv);
-const options: ScrapperOptions = program.opts();
+const options: ScrapperOptions & {
+  saveDataAsJson: boolean;
+} = program.opts();
 // finding offers
-const findOffers = async (searchValue: string, limitRecords: number) => {
+const findOffers = async (searchValue: string, limitRecords: number, saveDataAsJson = true) => {
   console.log("Scrapping...");
   const fileID = getCurrentDateAsUUID();
   const bot = new Bot({ searchValue, limitRecords });
@@ -21,16 +24,19 @@ const findOffers = async (searchValue: string, limitRecords: number) => {
   // finished scraping part:
   console.log(`${result.length} offers found`);
   const savePath = `./scrap-results/${fileID}`;
-  // save to JSON file
-  fileSystem.writeFile(`${savePath}.json`, JSON.stringify(result), (error) => error && console.log(error));
-  // save to CSV file
-  const headers = Object.keys(result[0]).map((key) => ({ id: key, title: key }));
-  const csvObject = csvWriter.createObjectCsvWriter({
-    path: `${savePath}.csv`,
-    header: headers,
-  });
-  csvObject.wri;
-  csvObject.writeRecords(result).catch((error) => console.log(error));
+  if (saveDataAsJson) {
+    // save to JSON file
+    fileSystem.writeFile(`${savePath}.json`, JSON.stringify(result), (error) => error && console.log(error));
+  } else {
+    // save to CSV file
+    const headers = Object.keys(result[0]).map((key) => ({ id: key, title: key }));
+    const csvObject = csvWriter.createObjectCsvWriter({
+      path: `${savePath}.csv`,
+      header: headers,
+    });
+    csvObject.wri;
+    csvObject.writeRecords(result).catch((error) => console.log(error));
+  }
 };
 
-findOffers(options.searchValue, options.limitRecords);
+findOffers(options.searchValue, options.limitRecords, options.saveDataAsJson);
